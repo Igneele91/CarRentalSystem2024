1e61a9e238333e37d58cdd303ce9275f83ac54de
Igneele <69846931+Igneele91@users.noreply.github.com>
2024-01-14 01:25:56 +0100

    Rotues funktionieren und css dateien
    farben und css und dann die rotues gehen auch fehler korrigiert.

--- AngularFrontendCarRental2024/src/app/app.routes.ts
+++ AngularFrontendCarRental2024/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { CarsComponent } from './app/cars/cars.component';
-import { CustomersComponent } from './app/customers/customers.component';
-import { RentalsComponent } from './app/rentals/rentals.component';
+import { AppComponent } from './app.component'; // Der Pfad muss ggf. angepasst werden
+import { CarsComponent } from './cars/cars.component';
+import { CustomersComponent } from './customers/customers.component';
+import { RentalsComponent } from './rentals/rentals.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent }, // Verwende AppComponent als Startseite
+  { path: '', component: AppComponent }, // AppComponent als Startseite
   { path: 'cars', component: CarsComponent },
   { path: 'customers', component: CustomersComponent },
   { path: 'rentals', component: RentalsComponent },
-  { path: '**', redirectTo: '/', pathMatch: 'full' } // Weiterleitung auf die Startseite bei unbekannten Pfaden
+  { path: '**', redirectTo: '/' } // Weiterleitung auf die Startseite bei unbekannten Pfaden
 ];
